dae215fc2ff93ecd3393b9b55350eac485820462
JasonCubed132 <amcb2002@googlemail.com>
2023-05-04 21:23:15 +0100

    Fix type issues

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,9 +3,9 @@
 
 use anyhow::Result;
 use dcspkg::config::{DcspkgConfig, DCSPKG_DIR};
+use dcspkg::util::list_installed_packages;
 use dcspkg::Package;
 use dcspkg::{install_package, list_all_packages, run_package};
-use dcspkg::util::list_installed_packages;
 
 lazy_static::lazy_static! {
     static ref DCSPKG_CONFIG: Result<DcspkgConfig, String> = match dcspkg::config::DcspkgConfig::get() {
@@ -27,7 +27,7 @@ fn get_all_games() -> Result<Vec<Package>, String> {
         Err(e) => {
             println!("{:#?}", e);
             Err("Failed to get games list".to_string())
-        },
+        }
     }
 }
 
@@ -39,7 +39,7 @@ fn get_installed_games() -> Result<Vec<Package>, String> {
         Err(e) => {
             println!("{:#?}", e);
             Err("Failed to get installed games list".to_string())
-        },
+        }
     }
 }
 
@@ -62,11 +62,8 @@ fn install_game(pkgname: &str) -> Result<(), String> {
         Ok(_) => Ok(()),
         Err(e) => {
             println!("{:#?}", e);
-            Err(format!(
-                "Failed to install {}",
-                pkgname_str2
-            ))
-        },
+            Err(format!("Failed to install {}", pkgname_str2))
+        }
     }
 }
 
@@ -75,12 +72,16 @@ fn run_game(pkgname: &str) -> Result<(), String> {
     let c = (*DCSPKG_CONFIG).clone()?;
     let pkgname_str = String::from(pkgname);
     let pkgname_str2 = pkgname_str.clone();
-    match run_package(&c.registry.registry_file, c.registry.install_dir, &pkgname_str) {
+    match run_package(
+        &c.registry.registry_file,
+        c.registry.install_dir,
+        &pkgname_str,
+    ) {
         Ok(_) => Ok(()),
         Err(e) => {
             println!("{:#?}", e);
             Err(format!("Failed to run {}", pkgname_str2))
-        },
+        }
     }
 }
 
@@ -90,15 +91,17 @@ fn main() {
     // some point.
 
     //create the dcspkg directory
-    std::fs::create_dir_all(&*DCSPKG_DIR)?;
+    std::fs::create_dir_all(&*DCSPKG_DIR)
+        .expect("Could not create dcspkg directory");
 
     //load config
-    let config = DcspkgConfig::get()?;
+    let config = DcspkgConfig::get()
+        .expect("Could not create config file");
 
     //create registry file if not exist
     if !config.registry.registry_file.is_file() {
         std::fs::write(&config.registry.registry_file, "[]")
-            .context("Could not create empty package registry file")?;
+            .expect("Could not create empty package registry file");
     }
 
     // Init Tauri
